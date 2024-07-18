3e99e49830cb9fd182094667c5a532f9406fe648
Zachary Baca <zachary.baca@msn.com>
2024-07-18 11:27:03 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
+import React from "react";
 import ToolContainer from "./components/ToolContainer/ToolContainer";
 import './App.css'
 
 function App() {
-  
+  const [color, setColor] = React.useState("#FF0000");
+
+  function handleChange(e) {
+    const {name, value} = e.target;
+    setColor(...prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+    
+  }
 
   return (
     <div id="app-container">
-      <h1>CSS Gradient Code Generator</h1>
-      <ToolContainer />
+      <h1 id="main-heading">CSS Gradient Code Generator</h1>
+      <ToolContainer color={color} setColor={setColor} handleChange={handleChange}/>
     </div>
   )
 }
