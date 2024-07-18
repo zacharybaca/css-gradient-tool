5519bdb27a8a5bb05dc7f5f614048bc8df865658
Zachary Baca <zachary.baca@msn.com>
2024-07-18 11:28:26 -0500

    Update Options.jsx

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,9 +1,21 @@
 import "./options.css";
 
-export default function Options() {
+
+export default function Options(props) {
+    
     return (
         <div id="options-container">
-
+            <div id="options-heading-container">
+                <h1 id="options-heading">Options</h1>
+            </div>
+            
+            <div id="color-info-container">
+                <div id="color-options-container">
+                    <h4>Color 1</h4>
+                    <h4>{props.color}</h4>
+                    <input type="color" name="color1" value={props.color} onChange={props.handleChange}/>
+                </div>
+            </div>
         </div>
     )
 }
