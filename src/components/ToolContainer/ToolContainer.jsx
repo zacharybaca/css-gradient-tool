5fa0b40f6fb15d3e58ca556b81cbc2ff32a6c743
Zachary Baca <zachary.baca@msn.com>
2024-07-18 11:30:52 -0500

    Update ToolContainer.jsx

--- src/components/ToolContainer/ToolContainer.jsx
+++ src/components/ToolContainer/ToolContainer.jsx
@@ -2,11 +2,11 @@ import Preview from "../Preview/Preview";
 import Options from "../Options/Options";
 import "./tool-container.css";
 
-export default function ToolContainer() {
+export default function ToolContainer(props) {
     return (
         <div id="tool-container">
-            <Preview />
-            <Options />
+            <Preview color={props.color}/>
+            <Options color={props.color} setColor={props.setColor} handleChange={props.handleChange}/>
         </div>
     )
 }
