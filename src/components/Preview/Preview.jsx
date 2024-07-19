9ea6e12f35bb7f689272c1eb09be70f362a1a8c3
Zachary Baca <zachary.baca@msn.com>
2024-07-19 11:41:46 -0500

    Update Preview.jsx

--- src/components/Preview/Preview.jsx
+++ src/components/Preview/Preview.jsx
@@ -1,6 +1,16 @@
 import "./preview.css";
 
 export default function Preview(props) {
+
+    const value = {
+        background: `background: linear-gradient(${props.color.angle}deg, ${props.color.color1}, ${props.color.color2}),
+        -moz-background': linear-gradient(${props.color.angle}deg, ${props.color.color1}, ${props.color.color2}),
+        -webkit-background': linear-gradient(${props.color.angle}deg, ${props.color.color1}, ${props.color.color2}),
+        
+        // for 3 color inputs
+        background: linear-gradient(${props.color.angle}deg, ${props.color.color1}, ${props.color.color2}, ${props.color.color3})`
+    };
+    
     return (
         <div id="preview-container">
             <div id="gradient-preview-container">
@@ -9,8 +19,8 @@ export default function Preview(props) {
                 </div>
             </div>
             <div id="generated-code-container">
-                <textarea id="generated-code">
-
+                <textarea id="generated-code" value={value.background} readOnly>
+                   
                 </textarea>
             </div>
         </div>
